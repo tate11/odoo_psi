5399f9ef5a6dd764f43cbd64d367fe2d5c4afe77
hlalarimina <hasiniaina.lalarimina@gmail.com>
2017-10-02 11:12:13 +0300

    account_analytic_account dans manifest

--- custom_modules/hr_recruitment_psi/__manifest__.py
+++ custom_modules/hr_recruitment_psi/__manifest__.py
@@ -6,7 +6,7 @@
     'sequence': 0,
     'description': """Gestion de recrutement PSI""",
     'website': 'https://www.ingenosya.mg',
-    'depends': ['hr','hr_recruitment','account.analytic.account'],
+    'depends': ['hr','hr_recruitment','account_analytic_account'],
     'data': [
              'data/hr_recruitment_data.xml',
              'views/hr_job_views.xml',
