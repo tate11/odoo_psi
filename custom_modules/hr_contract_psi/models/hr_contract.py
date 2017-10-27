c91c75de9a7d617da23b4ae70cf3688ea63e4e77
mifanomezantsoa <mifanomezantsoa@ingenosya.mg>
2017-10-27 09:15:15 +0300

    ADD : Suivi fin essai OK/KO

--- custom_modules/hr_contract_psi/models/hr_contract.py
+++ custom_modules/hr_contract_psi/models/hr_contract.py
@@ -22,12 +22,51 @@ class hr_contract(models.Model):
                                       ], string="Motif de rupture de contrat", track_visibility="onchange")
     
     work_years = fields.Integer(compute="_calculate_work_years", string=u'Année de travail')
+    
+    result_evaluation = fields.Selection([
+                                          ('ok','OK'),
+                                          ('ko','KO')
+                                          ], string="Résultat de l'évaluation")
+    
+    scan_version_file = fields.Binary(string=u'Attacher le version scanner')
 
     psi_contract_type = fields.Selection([
         ('cdd', 'CDD'),
         ('cdi', 'CDI'),
         ('convention_stage','Convention de stage')
     ], string='Type de contrat', help="Type de contrat", track_visibility='onchange')
+
+    @api.multi
+    def action_result_evaluation_send(self):
+        self.ensure_one()
+        ir_model_data = self.env['ir.model.data']
+        try:
+            template_id = ir_model_data.get_object_reference('hr_contract', 'email_template_for_result_evaluation')[1]
+        except ValueError:
+            template_id = False
+        try:
+            compose_form_id = ir_model_data.get_object_reference('mail', 'email_compose_message_wizard_form')[1]
+        except ValueError:
+            compose_form_id = False
+        ctx = dict()
+        ctx.update({
+            'default_model': 'hr.contract',
+            'default_res_id': self.ids[0],
+            'default_use_template': bool(template_id),
+            'default_template_id': template_id,
+            'default_composition_mode': 'comment',
+            'mark_so_as_sent': True
+        })
+        return {
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(compose_form_id, 'form')],
+            'view_id': compose_form_id,
+            'target': 'new',
+            'context': ctx,
+        }
 
     def _send_email_birthday_date_tracking(self):
         employee_obj = self.env['hr.contract']
