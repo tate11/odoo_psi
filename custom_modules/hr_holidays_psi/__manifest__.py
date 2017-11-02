e7096848e61da014cc6bbfc0f66e336c75c513e2
hlalarimina <hasiniaina.lalarimina@gmail.com>
2017-11-02 18:21:34 +0300

    ADD : structure hr_holidays

---
+++ custom_modules/hr_holidays_psi/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Gestion des absences PSI',
+    'version': '1.0',
+    'category': 'Human Resources',
+    'sequence': 0,
+    'description': """Gestion des absences PSI""",
+    'website': 'https://www.ingenosya.mg',
+    'depends': ['hr_holidays'],
+    'data': [
+        'data/hr_holidays_data.xml',
+        ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ custom_modules/hr_holidays_psi/models/hr_holidays_psi.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models,fields
+
+class hr_holidays_psi(models.Model):
+    _inherit = "hr.holidays"
+
