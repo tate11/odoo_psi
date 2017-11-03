33ed62af296e6c868714b68ba4904664efc75fbc
hlalarimina <hasiniaina.lalarimina@gmail.com>
2017-11-03 18:46:07 +0300

    ADD : droits congés

--- custom_modules/hr_holidays_psi/models/hr_holidays_configuration.py
+++ custom_modules/hr_holidays_psi/models/hr_holidays_configuration.py
@@ -6,5 +6,5 @@ class hr_holidays_holidays(models.Model):
     
     _name = "hr.holidays.configuration"
     
-    droit_conge = fields.Integer(string=u'Droits aux congés')
+    droit_conge = fields.Integer(string=u'Droits aux congés (en mois)')
     conges_sans_solde = fields.Integer(string=u'Congés sans soldes')
