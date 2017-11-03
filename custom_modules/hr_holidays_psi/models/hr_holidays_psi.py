c447549e5c09a2c91fa812698640d908f2f9136c
mifanomezantsoa <mifanomezantsoa@ingenosya.mg>
2017-11-03 17:28:45 +0300

    ADD : Conge maladie

--- custom_modules/hr_holidays_psi/models/hr_holidays_psi.py
+++ custom_modules/hr_holidays_psi/models/hr_holidays_psi.py
@@ -11,7 +11,9 @@ class hr_holidays_psi(models.Model):
 
     justificatif_file = fields.Binary(string=u'Pièce justificatif', help=u"Joindre un certificat médical ou une ordonnance", tracability="onchange") 
  
-    # Send mail - rappel piece justificatif - congé maladie  
+#    color_name_holidays_status = fields.Selection(related='holiday_status_id.color_name', string="color")
+    
+    # Send mail - rappel piece justificatif - conge maladie  
     @api.multi
     @api.constrains('holiday_status_id')  
     def _send_email_rappel_justificatif_conge_maladie(self, automatic=False):
@@ -27,7 +29,7 @@ class hr_holidays_psi(models.Model):
         date_to_notif = date_y_m_d + relativedelta(hours=48)   
         if self.id != False :
             for record in self:
-                if record.holiday_status_id.color_name == 'blue' or record.holiday_status_id.name == "Congé maladie":
+                if record.holiday_status_id.color_name == 'blue' or record.holiday_status_id.name == u"Cong� maladie":
 #                    if not record.justificatif_file:
                     if not self.justificatif_file and date_to_notif.date() == datetime.today().date() :
                         template = self.env.ref('hr_holidays_psi.custom_template_rappel_justificatif_conge_maladie')
