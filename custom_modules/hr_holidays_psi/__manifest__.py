cbd4d23a9f16dd692928813ff044b7e8f347bae5
mifanomezantsoa <mifanomezantsoa@ingenosya.mg>
2017-11-03 17:11:57 +0300

    ADD : absences conges maladie

--- custom_modules/hr_holidays_psi/__manifest__.py
+++ custom_modules/hr_holidays_psi/__manifest__.py
@@ -8,7 +8,10 @@
     'website': 'https://www.ingenosya.mg',
     'depends': ['hr_holidays'],
     'data': [
+        'data/hr_holidays_cron.xml',
         'data/hr_holidays_data.xml',
+        'data/mail_template_hr_holidays_data.xml',
+        'views/hr_holidays_views.xml',
         ],
     'demo': [],
     'test': [],
--- custom_modules/hr_holidays_psi/models/hr_holidays_psi.py
+++ custom_modules/hr_holidays_psi/models/hr_holidays_psi.py
@@ -1,7 +1,36 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models,fields
+import dateutil.parser
+from datetime import date, datetime
+from datetime import timedelta
+from dateutil.relativedelta import relativedelta
+from odoo import api, fields, models, _
 
 class hr_holidays_psi(models.Model):
     _inherit = "hr.holidays"
 
+    justificatif_file = fields.Binary(string=u'Pièce justificatif', help=u"Joindre un certificat médical ou une ordonnance", tracability="onchange") 
+ 
+    # Send mail - rappel piece justificatif - congé maladie  
+    @api.multi
+    @api.constrains('holiday_status_id')  
+    def _send_email_rappel_justificatif_conge_maladie(self, automatic=False):
+        print "test cron by send mail"
+        date_debut = self.date_from
+        date = dateutil.parser.parse(date_debut).date()
+        dt = datetime.strptime(date_debut,'%Y-%m-%d %H:%M:%S')
+        date_y_m_d = datetime(
+                                     year=date.year, 
+                                     month=date.month,
+                                     day=date.day,
+                )   
+        date_to_notif = date_y_m_d + relativedelta(hours=48)   
+        if self.id != False :
+            for record in self:
+                if record.holiday_status_id.color_name == 'blue' or record.holiday_status_id.name == "Congé maladie":
+#                    if not record.justificatif_file:
+                    if not self.justificatif_file and date_to_notif.date() == datetime.today().date() :
+                        template = self.env.ref('hr_holidays_psi.custom_template_rappel_justificatif_conge_maladie')
+                        self.env['mail.template'].browse(template.id).send_mail(self.id)               
+        if automatic:
+            self._cr.commit()
