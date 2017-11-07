db50d9caadb7464fd6c8277319e2681d21c22f82
jims <arandriamoravelo@ingenosya.mg>
2017-11-07 15:33:41 +0300

    FIX : demande de absences

--- custom_modules/hr_holidays_psi/report/report_request_for_absences.py
+++ custom_modules/hr_holidays_psi/report/report_request_for_absences.py
@@ -5,16 +5,13 @@ from odoo import api, models
 class report_request_for_absences(models.AbstractModel):
     _name = 'report.hr_holidays_psi.report_request_for_absences'
    
+   
+    def _get_all_absences(self, employee_id):
+        return self.env['hr.holidays'].search([('employee_id', '=', employee_id)])
+    
     @api.multi
     def render_html(self, docids, data=None):
-        #=======================================================================
-        # self.model = self.env.context.get('active_model')
-        # docs = self.env[self.model].browse(self.env.context.get('active_ids', []))
-        #=======================================================================
         
-        def _get_all_absences(self, employee_id):
-            return self.env['hr.holidays'].search([('employee_id', '=', employee_id)])
-            
         docargs = {
             'doc_ids': docids,
             'doc_model': 'hr.holidays',
