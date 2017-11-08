4a34e8f6953767ab67a8aba2715e0d71aa4fcf69
jims <arandriamoravelo@ingenosya.mg>
2017-11-08 15:24:48 +0300

    FIX : print

--- custom_modules/website_psi_job_recruitment/controllers/main.py
+++ custom_modules/website_psi_job_recruitment/controllers/main.py
@@ -341,7 +341,7 @@ class WebsiteHrRecruitment(http.Controller):
         linguistic_knowledge_line = {}
         if len(linguistics) > 0 and linguistics[0] != '' and writtens[0] != '' and spokens[0] != '' and listens[0] != '' :
             for index, linguistic in enumerate(linguistics):
-                print "AIZA E : ",linguistics[0]
+                
                 if  linguistics[index] != '' and writtens[index] != '' and spokens[index] != '' and listens[index] != '' :
                     linguistic_knowledge_line = {
     
