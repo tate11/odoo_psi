746eff81d6e382c8e5c0cc530c33dc5a6f48f41f
hlalarimina <hasiniaina.lalarimina@gmail.com>
2017-11-15 19:38:26 +0300

    FIX : controles

--- custom_modules/hr_timesheet_psi/models/hr_timesheet.py
+++ custom_modules/hr_timesheet_psi/models/hr_timesheet.py
@@ -95,19 +95,24 @@ class AccountAnalyticLine(models.Model):
     def traiter_unit_amount(self,vals):
         unit_amount=vals.get('unit_amount');
         
-        if unit_amount>self.task_id.planned_hours:
-            raise Warning('La durée du Timesheet entrée est supérieure à celle mentionnée dans la tâche actuelle qui est {}!'.format(self.float_time_to_time(self.task_id.planned_hours)))
-            return False
+        if vals.get('project_id'):
+            project = self.env['project.project'].browse(vals.get('project_id'))
+            vals['account_id'] = project.analytic_account_id.id
         
-        if unit_amount>self.unit_amount:
-            total_planned_hours=0
-            current_timesheets=self.env['account.analytic.line'].search([['project_id','=',self.project_id.id],['task_id','=',self.task_id.id]])
-            for current_timesheet in current_timesheets:
-                total_planned_hours+=float(current_timesheet.unit_amount)
-            if (total_planned_hours+unit_amount-self.unit_amount)>self.task_id.planned_hours:
-                raise Warning(u'Il ne reste plus que {} de travail dans cette tâche'.format(self.float_time_to_time(self.task_id.planned_hours-total_planned_hours)))   
+        if vals.get('date'):
+            current_day=datetime.strptime(vals.get('date'), '%Y-%m-%d').strftime('%w')
+            if current_day == "6" or current_day == "0" :
+                raise Warning('Vous ne devez pas travailler le week-end!')   
                 return False
         
+        if vals.get('task_id'):
+            #unit_amount=self.unit_amount
+            tasks = self.env['project.task'].search([('id', '=', vals.get('task_id'))])
+            for task in tasks:
+                if unit_amount>task.planned_hours:
+                    raise Warning('La durée du Timesheet entrée est supérieure à celle mentionnée dans cette tâche qui est {}!'.format(self.float_time_to_time(task.planned_hours)))  
+                    return False
+                
         if len(str(unit_amount))>2:
             
             heure,min=str(unit_amount).split(".")
@@ -129,18 +134,23 @@ class AccountAnalyticLine(models.Model):
             vals['unit_amount']=float("{}.{}".format(heure,min))
                 
     @api.model
-    def create(self, vals): 
-        if vals.get('project_id'):
-            project = self.env['project.project'].browse(vals.get('project_id'))
-            vals['account_id'] = project.analytic_account_id.id
-        if vals.get('date'):
-            current_day=datetime.strptime(vals.get('date'), '%Y-%m-%d').strftime('%w')
-            if current_day == "6" or current_day == "0" :
-                raise Warning('Vous ne devez pas travailler le week-end!')   
-                return False
+    def create(self, vals):
         
+        if vals.get('task_id') and vals.get('project_id') and vals.get('unit_amount') and vals.get('unit_amount') != 0.0:
+            current_timesheets = self.env['account.analytic.line'].search([['project_id', '=', vals.get('project_id')], ['task_id', '=', vals.get('task_id')]])
+            task = self.env['project.task'].search([('id','=',vals.get('task_id'))])[0]
+            total_planned_hours = 0
+            for current_timesheet in current_timesheets:
+                total_planned_hours += float(current_timesheet.unit_amount)
+            
+            total_planned_hours += vals.get('unit_amount')
+                
+            if total_planned_hours > task.planned_hours:
+                raise Warning(u'Le nombre d\'heure pour cette tâche dépasse de {}'.format(self.float_time_to_time(total_planned_hours - task.planned_hours ))) 
+                return False
+            
         if vals.get('task_id'):
-            unit_amount=self.unit_amount
+            unit_amount=vals.get('unit_amount')
             tasks=self.env['project.task'].search([('id','=',vals.get('task_id'))])
             for task in tasks:
                 if unit_amount>task.planned_hours:
@@ -148,20 +158,12 @@ class AccountAnalyticLine(models.Model):
                     return False
             
         self.traiter_unit_amount(vals)
+        
         return super(AccountAnalyticLine, self).create(vals)
 
     @api.multi
     def write(self, vals):
-        if vals.get('project_id'):
-            project = self.env['project.project'].browse(vals.get('project_id'))
-            vals['account_id'] = project.analytic_account_id.id
         
-        if vals.get('date'):
-            current_day=datetime.strptime(vals.get('date'), '%Y-%m-%d').strftime('%w')
-            if current_day == "6" or current_day == "0" :
-                raise Warning('Vous ne devez pas travailler le week-end!')   
-                return False
-            
         if vals.get('task_id'):
             unit_amount=self.unit_amount
             tasks=self.env['project.task'].search([('id','=',vals.get('task_id'))])
@@ -171,19 +173,29 @@ class AccountAnalyticLine(models.Model):
                     return False
             
         if vals.get('unit_amount'):
-            employees=self.env['hr.employee'].search([('id','=',self.user_id.id)])
-            heure_par_jour=8.5
-            print "eto"
+            employees = self.env['hr.employee'].search([('id', '=', self.env.user.id)])
+            heure_par_jour = 0.0
             for employee in employees:
-                attendance_ids=employee.calendar_id.attendance_ids
+                attendance_ids = employee.calendar_id.attendance_ids
                 for attendance_id in attendance_ids:
-                    attendances=self.env['resource.calendar.attendance'].search([['id','=',attendance_id.id],['dayofweek','=',datetime.strptime(self.date, '%Y-%m-%d').strftime('%w')]])
+                    attendances = self.env['resource.calendar.attendance'].search([['id', '=', attendance_id.id], ['dayofweek', '=', datetime.strptime(self.date, '%Y-%m-%d').strftime('%w')]])
                     for attendance in attendances:
-                        heure_par_jour+=attendance.hour_to-attendance.hour_from
+                        heure_par_jour += attendance.hour_to - attendance.hour_from
             
             if vals.get('unit_amount')>heure_par_jour:
                 raise Warning('Vous ne pouvez pas travailler plus de {} aujourd\'hui!'.format(self.float_time_to_time(heure_par_jour)))
                 return False
-            
-            self.traiter_unit_amount(vals)
+        
+            if vals.get('unit_amount') > self.unit_amount:
+                    total_planned_hours = 0
+                    current_timesheets = self.env['account.analytic.line'].search([['project_id', '=', self.project_id.id], ['task_id', '=', self.task_id.id]])
+                    for current_timesheet in current_timesheets:
+                        total_planned_hours += float(current_timesheet.unit_amount)
+                        
+                    if (total_planned_hours + vals.get('unit_amount') - self.unit_amount) > self.task_id.planned_hours:
+                        raise Warning(u'Le nombre d\'heure pour cette tâche dépasse de {}'.format(self.float_time_to_time((total_planned_hours + vals.get('unit_amount') - self.unit_amount) - self.task_id.planned_hours)))
+                        return False
+                    
+        self.traiter_unit_amount(vals)
+        
         return super(AccountAnalyticLine, self).write(vals)
