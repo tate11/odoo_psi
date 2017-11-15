ebd9851153539a27c9780754fa27bc467ca7a8f2
hlalarimina <hasiniaina.lalarimina@gmail.com>
2017-11-15 15:26:54 +0300

    ADD : security csv

--- custom_modules/hr_timesheet_psi/__manifest__.py
+++ custom_modules/hr_timesheet_psi/__manifest__.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Gestion des Timesheet PSI',
+    'name': 'Gestion des Timesheets PSI',
     'version': '1.0',
     'category': 'Timesheet',
     'sequence': 0,
     'description': """Gestion des Timesheet chez PSI-M""",
     'website': 'https://www.ingenosya.mg',
     'depends': ['account', 'hr', 'project'],
-    'data':  ['data/resource_calendar.xml'],
+    'data':  [
+        'data/resource_calendar.xml',
+        'security/ir.model.access.csv',
+        ],
     'demo': [],
     'test': [],
     'installable': True,
