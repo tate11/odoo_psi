1348f2fe42ffd2719b1630854db83a403b9f44a9
fsoloniaina <fsoloniaina@ingenosya.mg>
2017-11-22 09:51:38 +0300

    FIX : wkf_open_to_validation_finance

--- custom_modules/hr_recruitment_psi/models/hr_job.py
+++ custom_modules/hr_recruitment_psi/models/hr_job.py
@@ -105,57 +105,34 @@ class hr_job(models.Model):
         if vals.get('documents_count') == 0:
             raise Warning(u"Vous devez ajouter le fichier TDR.")
             return False
-        if not vals.get('tdr_add'):
-            raise Warning(u"Vous devez cochez sur TDR et en ajouter une pièce jointe")
-            return False
-        if not vals.get('experience_required_ids'):
-            raise Warning(u"Vous devez ajouter au moins un élément dans 'Expériences requises'")
-            return False
-        
-        if vals.get('nature_recrutement') and vals.get('nature_recrutement')=="conssideration_dossier":
-            if not vals.get('psi_memo'):
-                raise Warning("Vous devez cochez sur Memo")
-                return False
-            
         res = super(hr_job, self).create(vals)
         return res
     
-    @api.multi 
-    def write(self,vals):
+    def wkf_validation_finance(self):
         message=""
-        if vals.get('tdr_add') is None:
-            if not self.tdr_add:
-                message=u"Vous devez cochez sur TDR et en ajouter une pièce jointe"
-        else:
-            if not vals.get('tdr_add'):
-                message=u"Vous devez cochez sur TDR et en ajouter une pièce jointe"
+        if not self.tdr_add:
+            message=u"Vous devez cochez sur TDR et en ajouter une pièce jointe"
         
-        if vals.get('experience_required_ids') is None:
-            if not self.experience_required_ids:
-                if message!="":
-                    message=u"{} et {}".format(message,u"vous devez ajouter au moins un élément dans 'Expériences requises'")
-                else:
-                    message=u"Vous devez ajouter au moins un élément dans 'Expériences requises'"
-        elif not vals.get('experience_required_ids'):
-            if message!="":
-                    message=u"{} et {}".format(message,u"vous devez ajouter au moins un élément dans 'Expériences requises'")
-            else:
-                message=u"Vous devez ajouter au moins un élément dans 'Expériences requises'"
-        
-        if vals.get('nature_recrutement') and vals.get('nature_recrutement')=="conssideration_dossier":
-            if not vals.get('psi_memo') and not self.psi_memo:
+        if self.nature_recrutement=="conssideration_dossier":
+            if not self.psi_memo:
                 if message!="":
                     message=u"{} et {}".format(message,u"vous devez cocher sur Memo")
                 else:
                     message=u"Vous devez cocher sur Memo"
-                      
+                    
+        if not self.experience_required_ids:
+            if message!="":
+                message=u"{} et {}".format(message,u"vous devez ajouter au moins un élément dans 'Expériences requises'")
+            else:
+                message=u"Vous devez ajouter au moins un élément dans 'Expériences requises'"
+        
+                          
         if message!="":
             raise Warning(message)
             return False
+        else:
+            self.write({'state':'validation_finance'})
         
-        res=super(hr_job,self).write(vals)
-        return res
-    
     def relance(self):
         
         ctx = dict()
