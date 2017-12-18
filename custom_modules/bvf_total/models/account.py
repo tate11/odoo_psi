95be4eb526f0eaebe7ad1ef30b4492247135e1cd
hlalarimina <hasiniaina.lalarimina@gmail.com>
2017-12-18 15:16:54 +0300

    FIX : default devise MGA

--- custom_modules/bvf_total/models/account.py
+++ custom_modules/bvf_total/models/account.py
@@ -5,8 +5,8 @@ from odoo import api,models,fields, exceptions
 class AccountJournal(models.Model):
     _inherit = "account.journal"
     
-    def _default_currency(self):
-        return self.env['res.currency'].search('name','=','MGA')[0].id
+    def _default_currency_id(self):
+        return self.env['res.currency'].search([('name', '=', 'MGA')])[0].id
     
-    currency_total_id = fields.Many2one('res.currency', string="Currency", oldname='currency', _default=_default_currency)
+    currency_id = fields.Many2one('res.currency', string="Currency", oldname='currency', default=_default_currency_id)
         
