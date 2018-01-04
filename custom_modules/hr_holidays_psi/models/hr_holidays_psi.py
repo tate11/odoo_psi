f1f3d467effbd73f936f7df21323fe9249e2539b
jims <arandriamoravelo@ingenosya.mg>
2018-01-04 10:19:21 +0300

    FIX : conges restants

--- custom_modules/hr_holidays_psi/models/hr_holidays_psi.py
+++ custom_modules/hr_holidays_psi/models/hr_holidays_psi.py
@@ -27,6 +27,36 @@ class hr_holidays_type_psi(models.Model):
     holidays_status_id_psi = fields.Integer(string=u"id type de congé psi")
     limit = fields.Boolean(string=u'Dépassement de limite autorisé ', readonly=True)
     is_not_limited_j3 = fields.Boolean(string=u'is_not_limited_j3')
+    @api.multi
+    def get_days(self, employee_id):
+        # need to use `dict` constructor to create a dict per id
+        result = dict((id, dict(max_leaves=0, leaves_taken=0, remaining_leaves=0, virtual_remaining_leaves=0)) for id in self.ids)
+
+        holidays = self.env['hr.holidays'].search([
+            ('employee_id', '=', employee_id),
+            ('state', 'in', ['confirm', 'validate1', 'validate']),
+            ('holiday_status_id', 'in', self.ids)
+        ])
+
+        for holiday in holidays:
+            status_dict = result[holiday.holiday_status_id.id]
+            if holiday.type == 'add':
+                if holiday.state == 'validate':
+                    # note: add only validated allocation even for the virtual
+                    # count; otherwise pending then refused allocation allow
+                    # the employee to create more leaves than possible
+                    status_dict['virtual_remaining_leaves'] += holiday.number_of_days_temp
+                    status_dict['max_leaves'] += holiday.employee_id.nombre_conge
+                    status_dict['remaining_leaves'] += holiday.number_of_days_temp
+            elif holiday.type == 'remove':  # number of days is negative
+                status_dict['virtual_remaining_leaves'] -= holiday.number_of_days_temp
+                if holiday.state == 'validate':
+                    status_dict['leaves_taken'] += holiday.number_of_days_temp
+                    status_dict['remaining_leaves'] -= holiday.number_of_days_temp
+        return result
+
+   
+    
     
     
 class hr_holidays_type_permission(models.Model):
